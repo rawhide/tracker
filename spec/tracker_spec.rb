8ce521ace4c5ed50591c0e2aa4da22ea78726464
runeleaf <runeleaf@gmail.com>
2015-05-08 15:49:29 +0900

    spec

--- spec/tracker_spec.rb
+++ spec/tracker_spec.rb
@@ -3,9 +3,9 @@ require 'spec_helper'
 describe Tracker do
   describe "Base", ".execute" do
     context "run" do
-      subject { Tracker::Base.execute no: "123412341231", company: "sagawa" }
-      it { should include "123412341231" }
-      it { should include "sagawa" }
+      subject { Tracker::Base.execute(no: "123412341231", company: "sagawa")[0].first }
+      it { should be_value "123412341231" }
+      it { should be_value "sagawa" }
     end
 
     context "does not run" do
@@ -15,9 +15,9 @@ describe Tracker do
 
     context "array run" do
       subject { Tracker::Base.execute no: "123412341231" }
-      it { should include "sagawa" }
-      it { should include "yamato" }
-      it { should include "yuusei" }
+      it { should be_a Array }
+      it { expect(subject[0]).to be_a Array }
+      it { expect(subject.count).to eq 3 }
     end
   end
 end
