54fb2dbd4881b28b4453372d464be69a323233df
k.f.sawada <k.f.sawada@gmail.com>
2015-06-19 17:15:28 +0900

    [#97351672] すべての日付に西暦を(推測して)つける

--- lib/tracker/api/formatter/date.rb
+++ lib/tracker/api/formatter/date.rb
@@ -1,24 +1,40 @@
+require 'date'
+
 module Tracker # :nodoc:
   module Api # :nodoc:
     class Formatter # :nodoc:
       class Date
         # @param str [String] 日付
-        # @return [String] m-d
+        # @return [String] Y-m-d
         def self.convert(str)
           obj = self.new
           obj.convert str
         end
 
         # @param str [String] 日付
-        # @return [String] m-d
+        # @return [String] Y-m-d
         def convert(str)
           str, tmp = str.to_s.split
           str = str.to_s
-          str = str.gsub(/[年月]/, "-").gsub("日", "")
-          str = str.gsub("/", "-")
-          str = str.gsub(/\d{4}-/, "")
-          str = "00-00" if str.empty?
-          str
+          str = str.gsub(/[年月\-]/, "/").gsub("日", "")
+          if str.scan("/").length == 2
+            date = ::Date.parse(str).to_s
+
+          # 西暦が設定されていない場合、今日から前後半年間に存在する日付の西暦をセットする
+          elsif str.scan("/").length == 1
+            today = ::Date.today
+            date = ::Date.parse(str)
+            half_year = 365/2
+            diff = date.yday - today.yday
+
+            if diff > half_year
+              date = (date - 365)
+            elsif diff < -(half_year)
+              date = (date + 365)
+            end
+          end
+
+          str = date.to_s
         end
       end
     end
